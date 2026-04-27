b655469f3ca682ff2f1326058ce49fe871f4fcc2
Niall Oswald <niall.oswald@hotmail.co.uk>
2026-04-27 17:39:06 +0100

    feat: implement custom jvp

--- src/clapeyron_jax/__init__.py
+++ src/clapeyron_jax/__init__.py
@@ -1,15 +1,13 @@
 """JAX-compatible wrappers for Clapeyron.jl."""
 
-import pyclapeyron as cl
-
 from .wrapper import create_jax_wrapper
 
 # Pressure-based bulk properties
-mass_density = create_jax_wrapper(cl.mass_density, "(),(),(n)->()")
+mass_density = create_jax_wrapper("mass_density", "(),(),(n)->()")
 
 # Volume-based bulk properties
-pressure = create_jax_wrapper(cl.pressure, "(),(),(n)->()")
+pressure = create_jax_wrapper("pressure", "(),(),(n)->()")
 
 # Multiphase properties
-bubble_pressure = create_jax_wrapper(cl.bubble_pressure, "(),(),(n)->(),(),(),(n)")
-saturation_pressure = create_jax_wrapper(cl.saturation_pressure, "(),()->(),(),()")
+bubble_pressure = create_jax_wrapper("bubble_pressure", "(),(n)->(),(),(),(n)")
+saturation_pressure = create_jax_wrapper("saturation_pressure", "(),()->(),(),()")
--- src/clapeyron_jax/wrapper.py
+++ src/clapeyron_jax/wrapper.py
@@ -3,25 +3,44 @@
 import equinox as eqx
 import jax
 import jax.numpy as jnp
+from juliacall import Main as jl
 
 from clapeyron_jax.utils import parse_symbolic_shape, unwrap_scalar
 
-# from clapeyron_jax.config import JAX_ENABLE_X64
+jl.seval("using Clapeyron, ForwardDiff")
+
+jl.seval("""
+function jvp_wrapper(func, model, primals, tangents, kwargs)
+    function callback(ε)
+        perturbed_args = [p .+ ε .* t for (p, t) in zip(primals, tangents)]
+        return func(model, perturbed_args...; kwargs...)
+    end
+
+    primal_out = callback(0.0)
+    tangent_out = ForwardDiff.derivative(callback, 0.0)
+
+    return primal_out, tangent_out
+end
+""")
+
+jvp_wrapper = jl.jvp_wrapper
 
 
 def create_jax_wrapper(
-    func,
-    signature,
+    func_name: str,
+    signature: str,
     dtype=jnp.float32,
 ):
     # TODO: Get default dtype from JAX config
 
+    func = getattr(jl.Clapeyron, func_name)
+
+    @eqx.filter_custom_jvp
     def wrapped(model, *args, **kwargs):
         # Find output shape using the symbolic signature
         result_shape_dtypes = parse_symbolic_shape(signature, args, dtype)
 
         def callback(args, kwargs):
-            # Unwrap 0-d JAX arrays
             args = [unwrap_scalar(arr) for arr in args]
             kwargs = {key: unwrap_scalar(arr) for key, arr in kwargs.items()}
 
@@ -39,6 +58,41 @@ def create_jax_wrapper(
         return eqx.filter_pure_callback(
             callback,
             args,
+            kwargs,
+            result_shape_dtypes=result_shape_dtypes,
+            vmap_method="sequential",
+        )
+
+    @wrapped.def_jvp
+    def wrapped_jvp(primals, tangents, **kwargs):
+        model, *args = primals
+        _, *t_args = tangents
+
+        # Find output shape using the symbolic signature
+        result_shape_dtypes = parse_symbolic_shape(signature, args, dtype)
+        result_shape_dtypes = (result_shape_dtypes, result_shape_dtypes)
+
+        def callback(args, t_args, kwargs):
+            args = [unwrap_scalar(arr) for arr in args]
+            t_args = [unwrap_scalar(arr) for arr in t_args]
+            kwargs = {key: unwrap_scalar(arr) for key, arr in kwargs.items()}
+
+            primal_out, tangent_out = jvp_wrapper(func, model, args, t_args, kwargs)
+
+            print(primal_out, tangent_out)
+
+            return jax.tree_util.tree_map(
+                lambda val, struct: jnp.asarray(val, dtype=struct.dtype).reshape(
+                    struct.shape
+                ),
+                (primal_out, tangent_out),
+                result_shape_dtypes,
+            )
+
+        return eqx.filter_pure_callback(
+            callback,
+            args,
+            t_args,
             kwargs,
             result_shape_dtypes=result_shape_dtypes,
             vmap_method="sequential",
