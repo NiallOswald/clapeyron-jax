ac3493a47ffc6145f0edd139b7872cc245c4038c
Niall Oswald <niall.oswald@hotmail.co.uk>
2026-05-05 14:03:43 +0100

    refactor: move Phase enum to cljax package

--- src/clapeyron_jax/__init__.py
+++ src/clapeyron_jax/__init__.py
@@ -1,6 +1,19 @@
 """JAX-compatible wrappers for Clapeyron.jl."""
 
+from enum import StrEnum, auto
+
 from .wrapper import create_jax_wrapper
+
+
+class Phase(StrEnum):
+    """Phases accepted by Clapeyron.jl."""
+
+    Unknown = auto()
+    Liquid = auto()
+    Vapor = auto()
+    Solid = auto()
+    Stable = auto()
+
 
 # Pressure-based bulk properties
 mass_density = create_jax_wrapper("mass_density", "(),(),(n)->()")
