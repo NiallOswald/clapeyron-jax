4946d3a884063f93fd059bf7abe5098b382aa315
Niall Oswald <niall.oswald@hotmail.co.uk>
2026-06-22 16:14:06 +0100

    tmp

--- scripts/autogen_bindings.py
+++ scripts/autogen_bindings.py
@@ -1,1 +1,30 @@
 """Generate JAX bindings for Clapeyron.jl."""
+
+from juliacall import Main as jl
+
+jl.seval("using Clapeyron")
+
+
+EXCLUDED_NAMES = []
+
+
+def get_objects():
+    exported_symbols = jl.Base.names(jl.Clapeyron)
+
+    funcs, models = [], []
+    for sym in exported_symbols:
+        sym = str(sym)
+
+        if sym in EXCLUDED_NAMES:
+            continue
+        if sym.startswith("@"):
+            continue
+
+        obj = getattr(jl.Clapeyron, sym)
+
+        if jl.isa(obj, jl.Function):
+            funcs.append(obj)
+        else:
+            models.append(obj)
+
+    return funcs, models
