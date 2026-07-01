5e6eef0438c4baf3f63ca986641e140653fc47b4
Niall Oswald <niall.oswald@hotmail.co.uk>
2026-07-01 16:40:32 +0100

    feat: provide single interpreter

---
+++ src/clapeyron_jax/_julia/__init__.py
@@ -0,0 +1,56 @@
+"""The Julia backend."""
+
+
+def _load_interpreter():
+    """Load and configure Julia interpreter."""
+    from juliacall import Main as jl
+
+    jl.seval("""
+    using Clapeyron, ForwardDiff
+
+    struct JAXTag end
+
+    unwrap_dual(x::ForwardDiff.Dual) =
+        (ForwardDiff.value(x), ForwardDiff.partials(x)[1])
+
+    unwrap_dual(x::AbstractArray{<:ForwardDiff.Dual}) =
+        (ForwardDiff.value.(x), getindex.(ForwardDiff.partials.(x), 1))
+
+    unwrap_dual(x::Tuple) =
+        map(unwrap_dual, x) |> y -> (first.(y), last.(y))
+
+    unwrap_dual(x::NamedTuple) = begin
+        y = map(unwrap_dual, values(x))
+        (
+            NamedTuple{keys(x)}(Tuple(first.(y))),
+            NamedTuple{keys(x)}(Tuple(last.(y)))
+        )
+    end
+
+    unwrap_dual(x) =
+        throw(ArgumentError("Unsupported type: $(typeof(x))"))
+
+    function jvp_wrapper(func, model, primals, tangents, kwargs)
+        # Create a tag for the Dual number
+        base_type = eltype(primals[1])
+        T = typeof(ForwardDiff.Tag(JAXTag(), base_type))
+
+        args = [
+            t === nothing ? p : ForwardDiff.Dual{T}.(p, t)
+            for (p, t) in zip(primals, tangents)
+        ]
+        kwargs = Dict(Symbol(k) => v for (k, v) in kwargs)
+
+        dual_result = func.(model, args...; kwargs...)
+
+        return unwrap_dual(dual_result)
+    end
+    """)
+
+    return jl
+
+    def __getattr__(self, name: str):
+        return getattr(self._interpreter, name)
+
+
+julia = _load_interpreter()
--- src/clapeyron_jax/wrapper.py
+++ src/clapeyron_jax/wrapper.py
@@ -3,53 +3,9 @@
 import equinox as eqx
 import jax
 import jax.numpy as jnp
-from juliacall import Main as jl
+from ._julia import julia as jl
 
 from clapeyron_jax.utils import parse_symbolic_shape
-
-jl.seval("""
-using Clapeyron, ForwardDiff
-
-struct JAXTag end
-
-unwrap_dual(x::ForwardDiff.Dual) =
-    (ForwardDiff.value(x), ForwardDiff.partials(x)[1])
-
-unwrap_dual(x::AbstractArray{<:ForwardDiff.Dual}) =
-    (ForwardDiff.value.(x), getindex.(ForwardDiff.partials.(x), 1))
-
-unwrap_dual(x::Tuple) =
-    map(unwrap_dual, x) |> y -> (first.(y), last.(y))
-
-unwrap_dual(x::NamedTuple) = begin
-    y = map(unwrap_dual, values(x))
-    (
-        NamedTuple{keys(x)}(Tuple(first.(y))),
-        NamedTuple{keys(x)}(Tuple(last.(y)))
-    )
-end
-
-unwrap_dual(x) =
-    throw(ArgumentError("Unsupported type: $(typeof(x))"))
-
-function jvp_wrapper(func, model, primals, tangents, kwargs)
-    # Create a tag for the Dual number
-    base_type = eltype(primals[1])
-    T = typeof(ForwardDiff.Tag(JAXTag(), base_type))
-
-    args = [
-        t === nothing ? p : ForwardDiff.Dual{T}.(p, t)
-        for (p, t) in zip(primals, tangents)
-    ]
-    kwargs = Dict(Symbol(k) => v for (k, v) in kwargs)
-
-    dual_result = func.(model, args...; kwargs...)
-
-    return unwrap_dual(dual_result)
-end
-""")
-
-jvp_wrapper = jl.jvp_wrapper
 
 
 def create_jax_wrapper(func_name: str, signature: str):
@@ -85,7 +41,7 @@ def create_jax_wrapper(func_name: str, signature: str):
         result_shape_dtypes = parse_symbolic_shape(signature, t_args)
 
         def callback(_args, _t_args, _kwargs):
-            _, _tangent_out = jvp_wrapper(func, model, _args, _t_args, _kwargs)
+            _, _tangent_out = jl.jvp_wrapper(func, model, _args, _t_args, _kwargs)
             return jax.tree_util.tree_map(lambda val: jnp.asarray(val), _tangent_out)
 
         tangent_out = eqx.filter_pure_callback(
