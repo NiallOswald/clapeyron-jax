fix: broadcasting over mixtures
Hotfix which requires that the last positional argument is always the
vector of mole fractions.

src/clapeyron_jax/wrapper.py
@@ -23,8 +23,24 @@ class create_jax_wrapper:
         return getattr(jl.Clapeyron, self.fn_name)
 
     def _callback(self, args, kwargs):
-        result = jl.broadcast(self._jl_fn, *args, **kwargs)
-        return jax.tree_util.tree_map(lambda val: jnp.asarray(val), result)
+        # TODO: Assumes that args[-1] == zs
+        *args, zs = args
+
+        match zs.shape:
+            case (_,):
+                # Not broadcasting: zs (1d) -> [zs]
+                result = jl.eval_fn(self._jl_fn, *args, (zs,), **kwargs)[0]
+                return jax.tree.map(lambda xs: jnp.array(xs), result)
+            case (1, _):
+                # Not broadcasting on zs: zs (2d) -> [*zs] == [zs[0]]
+                result = jl.eval_fn(self._jl_fn, *args, (zs[0],), **kwargs)
+                return jax.tree.map(lambda xs: jnp.array(xs), result)
+            case (_, _):
+                # Broadcasting on zs: zs (2d) -> [*zs]
+                result = jl.eval_fn(self._jl_fn, *args, [*zs], **kwargs)
+                return jax.tree.map(lambda *xs: jnp.array(xs), *result)
+            case _:
+                raise ValueError("Shape mismatch in wrapped callback")
 
     def _jvp_callback(self, primals, tangents, kwargs):
         _, tangent_out = jl.jvp_wrapper(self._jl_fn, primals, tangents, kwargs)